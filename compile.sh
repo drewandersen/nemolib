2fad3e52b8abcba0b636776ff2dcd88f1390288e
drewda81 <drewda81@gmail.com>
2017-01-08 21:07:54 -0800

    Added confirmation message to compile.sh

--- compile.sh
+++ compile.sh
@@ -5,5 +5,6 @@ javac ./src/nemolib/*.java -d out
 
 cd ./out/
 
+echo "Success! nemolib.jar has been created in ~/lib/"
 if [ ! -d ~/lib ] ; then mkdir ~/lib; fi
 jar cf ~/lib/nemolib.jar ./nemolib/*.class
